c6dd110a4fc436956634ed25f38f97fb62421e46
Laurine Merault <laurine.merault@viacesi.fr>
2019-05-07 16:43:45 +0200

    fonction statique

--- classes/Log.php
+++ classes/Log.php
@@ -2,11 +2,17 @@
 
 class Log
 {
-    public static function logWrite()
+    public static function logWrite($data)
     {
         // Chemin vers le répertoire 'logs'
         $directory = "/logs/";
-
+        // On définit le nom du fichier de log en fonction de la date du jour
         $file = date('Y-m-d') . ".logs";
+        // On définit le nom du fichier avec son chemin complet depuis la racine
+        $path = dirname(__DIR__) . $directory . $file
+        // Défintion horodatage du message d'erreur passé en argument
+        $data = date('H:i:s') . " " . $data;
+        // Création de "ressource" après ouverture du fichier horodaté
+        $handle = fopen($path, "a");
     }
 }
